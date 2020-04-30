da51cd02729a4a7bd59763eeb088360f6bc3553e
Nataly <safnat11@gmail.com>
2020-04-30 17:35:03 +0300

    Add search work

--- GithubSearch/Controllers/ViewController.swift
+++ GithubSearch/Controllers/ViewController.swift
@@ -10,14 +10,20 @@ import UIKit
 import Alamofire
 import SwiftyJSON
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController, UISearchBarDelegate {
+    
+    @IBOutlet weak var searchBar: UISearchBar!
     
     let URL = "https://api.github.com/search/repositories?"
     
+    var userText = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        requestInfo(with: "Alamofire")
+        
+        searchBar.delegate = self
+        
+       // requestInfo(with: "Alamofire")
     }
     
     
@@ -30,18 +36,44 @@ class ViewController: UIViewController {
         
         Alamofire.request(URL, method: .get, parameters: parameters).responseJSON { (response) in
             if response.result.isSuccess {
-                print(response.result)
-                
+               
                 //Use SwiftyJSON
-                let json: JSON = JSON(response.result.value!)
-                
-                for i in 0...25 {
-                    let result =  json ["items"][i]["full_name"]
-                    print(result)
+                if response.result.value != nil {
+                    let json: JSON = JSON(response.result.value!)
+                    
+                    for i in 0...25 {
+                        let result =  json ["items"][i]["full_name"]
+                        print(result)
+                    }
                 }
+                
             }
         }
     }
+    
+    //MARK: UISearchBarDelegate Methods
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        if let text = searchBar.text {
+            userText = text
+            requestInfo(with: userText)
+        }
+        searchBar.resignFirstResponder()
+           
+        
+        
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+//        userText = searchText
+//        print(userText)
+//        requestInfo(with: userText)
+        
+    }
+    
+    
+        
+    
     
     
 }
