d0efde22f51a57643164fb5ab278fce4c9208f98
Nataly <safnat11@gmail.com>
2020-04-30 15:20:21 +0300

    Finished working with the network

--- GithubSearch/Controllers/ViewController.swift
+++ GithubSearch/Controllers/ViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import Alamofire
+import SwiftyJSON
 
 class ViewController: UIViewController {
     
@@ -29,7 +30,15 @@ class ViewController: UIViewController {
         
         Alamofire.request(URL, method: .get, parameters: parameters).responseJSON { (response) in
             if response.result.isSuccess {
-                print(response)
+                print(response.result)
+                
+                //Use SwiftyJSON
+                let json: JSON = JSON(response.result.value!)
+                
+                for i in 0...25 {
+                    let result =  json ["items"][i]["full_name"]
+                    print(result)
+                }
             }
         }
     }
